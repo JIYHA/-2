2c54e7cccec11c65bed6906446a94da41c78abdd
ya.zmeya.2001@gmail.com <ya.zmeya.2001@gmail.com>
2019-10-08 21:27:32 +0300

    Добавьте файлы проекта.

---
+++ Задание 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan now = DateTime.Now.TimeOfDay;
+            int seconds = (int)now.TotalSeconds;
+            int hours = seconds / 3600;
+            int minutes = (seconds - (hours * 3600)) / 60;
+            Console.WriteLine("Hours: " + hours);
+            Console.WriteLine("Min: " + minutes);
+        }
+    }
+}
